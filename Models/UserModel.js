f9a77c741f7d13fdbfbcc4e1f983e7d97dcaedb6
joersaac <00159619@uca.edu.sv>
2021-11-19 23:42:21 -0600

    Add: models for pokemon and user

---
+++ Models/UserModel.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    admin: {
+        type: Boolean,
+        required: true,
+    },
+    favPkmn: [String],
+});
+
+module.exports = model('User', UserSchema);
